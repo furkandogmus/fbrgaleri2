9e6acef4c8ff69b006257237d207fcdea9511e64
root <root@local>
2024-02-23 23:42:43 +0300

    sixth

--- src/MyGallery.js
+++ src/MyGallery.js
@@ -3,16 +3,16 @@ import ImageGallery from 'react-image-gallery';
 
 const images = [
   {
-    original: 'https://i.postimg.cc/25YMWr8Y/1.png',
-    thumbnail: 'https://i.postimg.cc/25YMWr8Y/1.png',
+    original: 'https://iili.io/JGuRsQ1.png',
+    thumbnail: 'https://iili.io/JGuRsQ1.png',
   },
   {
-    original: 'https://i.postimg.cc/rmFHhjrY/2.png',
-    thumbnail: 'https://i.postimg.cc/rmFHhjrY/2.png',
+    original: 'https://iili.io/JGuRPTB.png',
+    thumbnail: 'https://iili.io/JGuRPTB.png',
   },
   {
-    original: 'https://i.postimg.cc/sDBkdT0k/3.png',
-    thumbnail: 'https://i.postimg.cc/sDBkdT0k/3.png',
+    original: 'https://iili.io/JGuRihP.png',
+    thumbnail: 'https://iili.io/JGuRihP.png',
   },
 ];
 
