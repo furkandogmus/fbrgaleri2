2da0a45a60ba657b2cd6767d4734e16b7b0bad46
root <root@local>
2024-02-24 00:20:00 +0300

    seventh

--- src/MyGallery.js
+++ src/MyGallery.js
@@ -14,6 +14,55 @@ const images = [
     original: 'https://iili.io/JGuRihP.png',
     thumbnail: 'https://iili.io/JGuRihP.png',
   },
+  {
+    original: 'https://iili.io/JGuMG5X.png',
+    thumbnail: 'https://iili.io/JGuMG5X.png',
+  },
+  {
+    original: 'https://iili.io/JGuMMen.png',
+    thumbnail: 'https://iili.io/JGuMMen.png',
+  },
+  {
+    original: 'https://iili.io/JGuMEJt.png',
+    thumbnail: 'https://iili.io/JGuMEJt.png',
+  },
+  {
+    original: 'https://iili.io/JGuM0gI.png',
+    thumbnail: 'https://iili.io/JGuM0gI.png',
+  },
+  {
+    original: 'https://iili.io/JGuMVbs.png',
+    thumbnail: 'https://iili.io/JGuMVbs.png',
+  },
+  {
+    original: 'https://iili.io/JGuMXzG.png',
+    thumbnail: 'https://iili.io/JGuMXzG.png',
+  },
+  
+  {
+    original: 'https://iili.io/JGuMO12.png',
+    thumbnail: 'https://iili.io/JGuMO12.png',
+  },
+  {
+    original: 'https://iili.io/JGuMNql.png',
+    thumbnail: 'https://iili.io/JGuMNql.png',
+  },
+  {
+    original: 'https://iili.io/JGuMegS.png',
+    thumbnail: 'https://iili.io/JGuMegS.png',
+  },
+  {
+    original: 'https://iili.io/JGuMvd7.png',
+    thumbnail: 'https://iili.io/JGuMvd7.png',
+  },
+  {
+    original: 'https://iili.io/JGuM879.png',
+    thumbnail: 'https://iili.io/JGuM879.png',
+  },
+  {
+    original: 'https://iili.io/JGuMSee.png',
+    thumbnail: 'https://iili.io/JGuMSee.png',
+  },
 ];
 
 const MyGallery = () => {
